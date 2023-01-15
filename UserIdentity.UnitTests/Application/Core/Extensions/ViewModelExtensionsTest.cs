f56bee8c41a0cf09f26d2010606086b89a89278b
Samuel Mwangi <mwangi.s.irungu@gmail.com>
2023-01-15 19:35:11 +0300

    Update VM extensions tests

--- UserIdentity.UnitTests/Application/Core/Extensions/ViewModelExtensionsTest.cs
+++ UserIdentity.UnitTests/Application/Core/Extensions/ViewModelExtensionsTest.cs
@@ -11,12 +11,23 @@ namespace UserIdentity.UnitTests.Application.Core.Extensions
 
     }
 
+    public record TestItemDetailBaseVM : ItemDetailBaseViewModel
+    {
+
+    }
+
+
+    public record TestItemsBaseVM : ItemsBaseViewModel
+    {
+
+    }
+
     public class ViewModelExtensionsTest
     {
         [Fact]
         public void Resolve_Request_Status_Updates_VM_Status()
         {
-            TestBaseVM vm = new TestBaseVM();
+            TestBaseVM vm = new();
 
             vm.ResolveRequestStatus(RequestStatus.SUCCESSFUL, ItemStatusMessage.SUCCESS);
 
@@ -27,7 +38,7 @@ namespace UserIdentity.UnitTests.Application.Core.Extensions
         [Fact]
         public void Resolve_StatusMessage_Status_Updates_VM_StatusMessage()
         {
-            TestBaseVM vm = new TestBaseVM();
+            TestBaseVM vm = new();
             String customMesage = "Failed but looks like success";
 
             vm.ResolveRequestStatus(RequestStatus.FAILED, ItemStatusMessage.SUCCESS, customMesage);
@@ -35,6 +46,46 @@ namespace UserIdentity.UnitTests.Application.Core.Extensions
             Assert.Equal(RequestStatus.FAILED.GetDisplayName(), vm.RequestStatus);
             Assert.Equal(customMesage, vm.StatusMessage);
         }
+
+        [Fact]
+        public void Resolve_EditDelete_Rights_Updates_EditDelete_Rights()
+        {
+            TestItemDetailBaseVM vm = new();
+            TestItemDetailBaseVM vm2 = new();
+
+            String targetEntity = "TestEntity";
+            String userRoleClaims = $"{targetEntity}:edit,{targetEntity}:delete"; 
+
+            vm.ResolveEditDeleteRights(userRoleClaims,targetEntity);
+            vm2.ResolveEditDeleteRights("", targetEntity);
+
+            Assert.True(vm.EditEnabled);
+            Assert.True(vm.DeleteEnabled);
+
+            Assert.False(vm2.EditEnabled);
+            Assert.False(vm2.DeleteEnabled);
+        }
+
+        [Fact]
+        public void Resolve_CreateDownload_Rights_Updates_CreateDownload_Rights()
+        {
+            TestItemsBaseVM vm = new();
+            TestItemsBaseVM vm2 = new();
+
+            String targetEntity = "TestEntity";
+            String userRoleClaims = $"{targetEntity}:create,{targetEntity}:download";
+
+            vm.ResolveCreateDownloadRights(userRoleClaims, targetEntity);
+            vm2.ResolveCreateDownloadRights("", targetEntity);
+
+            Assert.True(vm.CreateEnabled);
+            Assert.True(vm.DownloadEnabled);
+
+            Assert.False(vm2.CreateEnabled);
+            Assert.False(vm2.DownloadEnabled);
+        }
+
+
 
     }
 }
