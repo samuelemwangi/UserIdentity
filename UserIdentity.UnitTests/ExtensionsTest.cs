8576096f5bf06f6f7f23d8ac92e258c7bda0ffd2
Samuel Mwangi <mwangi.s.irungu@gmail.com>
2023-02-18 00:08:59 +0300

    Unit Tests -  Add Repos to DI Container

---
+++ UserIdentity.UnitTests/ExtensionsTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using UserIdentity.Persistence.Repositories.RefreshTokens;
+using UserIdentity.Persistence.Repositories.Users;
+using Xunit;
+using FakeItEasy;
+using UserIdentity.Persistence;
+
+namespace UserIdentity.UnitTests
+{
+	public class ExtensionsTest
+	{
+		private readonly IServiceCollection _services;
+		public ExtensionsTest()
+		{
+			_services = new ServiceCollection();
+		}
+
+		[Fact]
+		public void AddRepositories_to_DI_Container_Adds_Repositories_To_DI_Container()
+		{
+			// Act
+			_services.AddRepositories();
+
+			// Assert
+			Assert.Equal(2, _services.Count);
+
+			Assert.Contains(_services, s => s.ServiceType == typeof(IUserRepository));
+			Assert.Contains(_services, s => s.ServiceType == typeof(IRefreshTokenRepository));
+
+			Assert.Contains(_services, s => s.ImplementationType == typeof(UserRepository));
+			Assert.Contains(_services, s => s.ImplementationType == typeof(RefreshTokenRepository));
+		}
+	}
+}
+
