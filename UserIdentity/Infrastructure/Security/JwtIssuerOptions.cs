7b6cbffa6e63217d67e4c43c1c6cbb3f86e5133a
Samuel Mwangi <mwangi.s.irungu@gmail.com>
2023-02-26 13:47:43 +0300

    Add JWT Factory Unit tests

--- UserIdentity/Infrastructure/Security/JwtIssuerOptions.cs
+++ UserIdentity/Infrastructure/Security/JwtIssuerOptions.cs
@@ -2,35 +2,38 @@
 
 namespace UserIdentity.Infrastructure.Security
 {
-  public class JwtIssuerOptions
-  {
-    //iss -> Issuer claim identifies the princcipal that issued the JWT 
-    public string? Issuer { get; set; }
+    public class JwtIssuerOptions
+    {
+        //iss -> Issuer claim identifies the princcipal that issued the JWT 
+        public String? Issuer { get; set; }
 
-    //sub -> Subject identifies the subject of the JWT
-    public string? Subject { get; set; }
+        //sub -> Subject identifies the subject of the JWT
+        public String? Subject { get; set; }
 
-    //aud -> The recipients of the JWT
-    public string? Audience { get; set; }
+        //aud -> The recipients of the JWT
+        public String? Audience { get; set; }
 
-    //nbf -> JWT not valid before this time
-    public DateTime NotBefore => DateTime.UtcNow;
+        //nbf -> JWT not valid before this time
+        public DateTime NotBefore => DateTime.UtcNow;
 
-    //iat -> Time JWT was issued at 
-    public DateTime IssuedAt => DateTime.UtcNow;
+        //iat -> Time JWT was issued at 
+        public DateTime IssuedAt => DateTime.UtcNow;
 
-    //Timespan the JWT is valid for
-    public TimeSpan ValidFor { get; set; }
-    //= TimeSpan.FromMinutes(1);
+        //Timespan the JWT is valid for
+        public TimeSpan ValidFor { get; set; }
+        //= TimeSpan.FromMinutes(1);
 
-    //exp -> The Time after which JWT must not be accepted for processing
-    public DateTime Expiration => IssuedAt.Add(ValidFor);
+        //exp -> The Time after which JWT must not be accepted for processing
+        public DateTime Expiration => IssuedAt.Add(ValidFor);
 
 
-    // "jti" (JWT ID) Claim (default ID is a GUID)
-    public Func<Task<string>> JtiGenerator =>
-      () => Task.FromResult(Guid.NewGuid().ToString());
+        // "jti" (JWT ID) Claim (default ID is a GUID)
+        // add getter and setter for JtiGenerator
+        public Func<Task<String>> JtiGenerator =>
+          () => Task.FromResult(Guid.NewGuid().ToString());
 
-    public SigningCredentials? SigningCredentials { get; set; }
-  }
+        
+
+        public SigningCredentials? SigningCredentials { get; set; }
+    }
 }
