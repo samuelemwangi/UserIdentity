8df6b4bc415ac7f48736ff90f75e0139a01335fa
samuelemwangi <mwangi.s.irungu@gmail.com>
2023-03-04 15:04:34 +0300

    Update RefreshToken Configuration Tests

---
+++ UserIdentity.UnitTests/Persistence/Configurations/EntityConfigurationPropertiesTestsHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+
+namespace UserIdentity.UnitTests.Persistence.Configurations
+{
+	internal static class EntityConfigurationPropertiesTestsHelper
+	{
+
+		public static Boolean ConfirmMaxColumnLength(this EntityType entityType, String column, Int32 columnLength)
+		{
+			var properies = entityType.GetProperties();
+
+			foreach (var prop in properies)
+			{
+				if (prop.Name == column)
+				{
+					return prop.GetMaxLength() == columnLength;
+				}
+			}
+
+			return false;
+		}
+
+		public static Boolean ConfirmColumnHasIndex(this EntityType entityType, String column)
+		{
+			var properies = entityType.GetProperties();
+
+			foreach (var prop in properies)
+			{
+				if (prop.Name == column)
+				{
+					return prop.IsIndex();
+				}
+			}
+
+			return false;
+		}
+	}
+}
---
+++ UserIdentity.UnitTests/Persistence/Configurations/EntityConfigurationTestsHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace UserIdentity.UnitTests.Persistence.Configurations
+{
+	internal class EntityConfigurationTestsHelper<T> where T : class
+	{
+
+		public static (EntityTypeBuilder<T>, EntityType) GetEntityTypeBuilder()
+		{
+			EntityType enityType = new EntityType(nameof(T), typeof(T), new Model(), false, ConfigurationSource.Convention);
+			var builder = new EntityTypeBuilder<T>(enityType);
+			return (builder, enityType);
+		}
+
+	}
+}
