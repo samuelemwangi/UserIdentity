61d35014e7b05f54e28ccb6d80beb7f02585f56e
samuelemwangi <mwangi.s.irungu@gmail.com>
2023-03-13 02:10:02 +0300

    Update comment

--- UserIdentity/Properties/AssemblyInfo.cs
+++ UserIdentity/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Expose internals to unit tests
+// Expose internals to unit tests & integration tests
 [assembly: InternalsVisibleTo("UserIdentity.UnitTests")]
 [assembly: InternalsVisibleTo("UserIdentity.IntegrationTests")]
 
