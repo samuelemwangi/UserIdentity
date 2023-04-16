e232c1a38595151a4fbb7daf5c1004fbf6bed400
samuelemwangi <mwangi.s.irungu@gmail.com>
2023-04-16 14:20:14 +0300

    Better enums for status message

--- UserIdentity/Application/Enums/ItemStatusMessage.cs
+++ UserIdentity/Application/Enums/ItemStatusMessage.cs
@@ -4,13 +4,34 @@ namespace UserIdentity.Application.Enums
 {
 	public enum ItemStatusMessage
 	{
-		[Description("Item(s) fetched successfully")]
-		SUCCESS,
+		[Description("Item created successfully")]
+		CREATE_ITEM_SUCCESSFUL,
 
-		[Description("Fetching(s) item failed")]
-		FAILED,
 
-		[Description("The requested item(s) could not be found")]
-		NOTFOUND
+		[Description("Item updated successfully")]
+		UPDATE_ITEM_SUCCESSFUL,
+
+
+		[Description("Item fetched successfully")]
+		FETCH_ITEM_SUCCESSFUL,
+
+		[Description("Items fetched successfully")]
+		FETCH_ITEMS_SUCCESSFUL,
+
+		[Description("No items found")]
+		FETCH_ITEMS_SUCCESSFUL_NO_ITEMS,
+
+		[Description("Item deleted successfully")]
+		DELETE_ITEM_SUCCESSFUL,
+
+
+		[Description("Fetching item failed")]
+		FETCH_ITEM_FAILED,
+
+		[Description("Fetching items failed")]
+		FETCH_ITEMS_FAILED,
+
+		[Description("The requested item could not be found")]
+		ITEM_NOT_FOUND
 	}
 }
