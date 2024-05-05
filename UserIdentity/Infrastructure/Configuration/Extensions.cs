e703701aaa639a6c5503e09c3cbcefc8808acaee
samuelemwangi <mwangi.s.irungu@gmail.com>
2024-05-05 21:15:47 +0300

    add asymetric JWT signing

---
+++ UserIdentity/Infrastructure/Configuration/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using UserIdentity.Application.Exceptions;
+
+namespace UserIdentity.Infrastructure.Configuration
+{
+	public static class Extensions
+	{
+		public static string GetEnvironmentVariable(this IConfiguration configuration, string key)
+		{
+			var value = configuration[key];
+			if (string.IsNullOrEmpty(value))
+			{
+				throw new MissingConfigurationException(key);
+			}
+			return value;
+		}
+	}
+}
