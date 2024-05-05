92b00ad431a3ba532f9b39347106e28c65787cb3
samuelemwangi <mwangi.s.irungu@gmail.com>
2024-05-05 21:15:47 +0300

    add asymetric JWT signing

---
+++ UserIdentity/Infrastructure/Utilities/FileSystemKeyProvider.cs
@@ -0,0 +1,19 @@
+﻿using UserIdentity.Application.Interfaces.Utilities;
+
+namespace UserIdentity.Infrastructure.Utilities
+{
+	public class FileSystemKeyProvider : IKeyProvider
+	{
+		public async Task<string> GetKeyAsync(string keyName)
+		{
+			var path = keyName.Split("/").Aggregate(Path.Combine(Environment.CurrentDirectory), Path.Combine);
+
+			if (!File.Exists(path))
+			{
+				throw new FileNotFoundException($"File not found at {path}");
+			}
+
+			return await File.ReadAllTextAsync(path);
+		}
+	}
+}
