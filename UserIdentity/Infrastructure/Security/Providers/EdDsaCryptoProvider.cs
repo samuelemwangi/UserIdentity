1b1ff036324b46f896f9ce798737c622243f0231
samuelemwangi <mwangi.s.irungu@gmail.com>
2025-01-15 23:45:49 +0300

    use EdDSA

---
+++ UserIdentity/Infrastructure/Security/Providers/EdDsaCryptoProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+
+using Microsoft.IdentityModel.Tokens;
+
+namespace UserIdentity.Infrastructure.Security.Providers
+{
+	public class EdDsaCryptoProvider : ICryptoProvider
+	{
+		public object Create(string algorithm, params object[] args)
+		{
+			if (algorithm == EdDsaSecurityAlgorithmConstants.EdDsa && args[0] is EdDsaSecurityKey key)
+			{
+				return new EdDsaSignatureProvider(key, algorithm, (bool)args[1]);
+			}
+
+			throw new CryptographicException($"Unsupported algorithm: {algorithm}");
+		}
+
+		public bool IsSupportedAlgorithm(string algorithm, params object[] args) => algorithm == EdDsaSecurityAlgorithmConstants.EdDsa;
+
+		public void Release(object cryptoInstance)
+		{
+			if (cryptoInstance is IDisposable disposableObject)
+				disposableObject.Dispose();
+		}
+	}
+}
--- UserIdentity/Infrastructure/Security/Providers/EdDsaSignatureProvider.cs
+++ UserIdentity/Infrastructure/Security/Providers/EdDsaSignatureProvider.cs
@@ -22,14 +22,45 @@ namespace UserIdentity.Infrastructure.Security.Providers
 
 			_signer.Init(true, _edDsaKey.PrivateKey);
 			_signer.BlockUpdate(input, 0, input.Length);
+
 			return _signer.GenerateSignature();
+		}
+
+		public override byte[] Sign(byte[] input, int offset, int count)
+		{
+			if (!WillCreateSignatures)
+				throw new InvalidOperationException("Provider not configured for signing.");
+
+			_signer.Init(true, _edDsaKey.PrivateKey);
+			_signer.BlockUpdate(input, offset, count);
+			return _signer.GenerateSignature();
+		}
+
+		public override bool Sign(ReadOnlySpan<byte> data, Span<byte> destination, out int bytesWritten)
+		{
+			var signature = Sign(data.ToArray());
+			signature.CopyTo(destination);
+			bytesWritten = signature.Length;
+			return true;
 		}
 
 		public override bool Verify(byte[] input, byte[] signature)
 		{
 			_signer.Init(false, _edDsaKey.PublicKey);
+
 			_signer.BlockUpdate(input, 0, input.Length);
+
 			return _signer.VerifySignature(signature);
+		}
+
+		public override bool Verify(byte[] input, int inputOffset, int inputLength, byte[] signature, int signatureOffset, int signatureLength)
+		{
+			ArgumentNullException.ThrowIfNull(input);
+			ArgumentNullException.ThrowIfNull(signature);
+			if (inputLength <= 0) throw new ArgumentException($"{nameof(inputLength)} must be greater than 0");
+			if (signatureLength <= 0) throw new ArgumentException($"{nameof(signatureLength)} must be greater than 0");
+
+			return Verify(input.Skip(inputOffset).Take(inputLength).ToArray(), signature.Skip(signatureOffset).Take(signatureLength).ToArray());
 		}
 
 		protected override void Dispose(bool disposing)
