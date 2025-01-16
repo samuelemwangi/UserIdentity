d9fbd610d11ba291df4704963c3fdb29095ee083
samuelemwangi <mwangi.s.irungu@gmail.com>
2025-01-17 00:10:22 +0300

    refine JwtFactory class & update copyright

--- UserIdentity/Infrastructure/Security/JwtFactory.cs
+++ UserIdentity/Infrastructure/Security/JwtFactory.cs
@@ -1,119 +1,80 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Security.Principal;
 
 using Microsoft.Extensions.Options;
 
 using UserIdentity.Application.Interfaces.Security;
 using UserIdentity.Application.Interfaces.Utilities;
-using UserIdentity.Infrastructure.Security.Helpers;
 
 namespace UserIdentity.Infrastructure.Security
 {
 	public class JwtFactory : IJwtFactory
 	{
-		private readonly IJwtTokenHandler _jwtTokenHandler;
 		private readonly JwtIssuerOptions _jwtOptions;
 		private readonly IMachineDateTime _machineDateTime;
 
-		private char scopeClaimSeparator => ':';
+		private static readonly char _scopeClaimSeparator = ':';
 
-		public JwtFactory(IJwtTokenHandler jwtTokenHandler, IOptions<JwtIssuerOptions> jwtOptions, IMachineDateTime machineDateTime)
+		public JwtFactory(IOptions<JwtIssuerOptions> jwtOptions, IMachineDateTime machineDateTime)
 		{
-			_jwtTokenHandler = jwtTokenHandler;
 			_jwtOptions = jwtOptions.Value;
 			_machineDateTime = machineDateTime;
 			ThrowIfInvalidOptions(_jwtOptions);
-
 		}
 		public async Task<(string, int)> GenerateEncodedTokenAsync(string id, string userName, IList<string> userRoles, HashSet<string> userRoleClaims)
 		{
-			var identity = GenerateClaimsIdentity(id, userName);
-			var roleClaims = userRoles.Select(x => new Claim(Constants.Strings.JwtClaimIdentifiers.Rol, x)).ToArray();
-
-			var claims = new[]{
-				new Claim(JwtRegisteredClaimNames.Sub, userName),
-				new Claim(JwtRegisteredClaimNames.Jti, await _jwtOptions.JtiGenerator()),
-				new Claim(JwtRegisteredClaimNames.Iat, _machineDateTime.ToUnixEpochDate(_jwtOptions.IssuedAt).ToString(), ClaimValueTypes.Integer64),
-				identity.FindFirst(Constants.Strings.JwtClaimIdentifiers.Id),
+			var claims = new List<Claim>{
+				new(JwtRegisteredClaimNames.Sub, userName),
+				new(JwtRegisteredClaimNames.Jti, await _jwtOptions.JtiGenerator()),
+				new(JwtRegisteredClaimNames.Iat, _machineDateTime.ToUnixEpochDate(_jwtOptions.IssuedAt).ToString(), ClaimValueTypes.Integer64),
+				new(JwtCustomClaimNames.Id, id)
 			};
 
+			claims.AddRange(userRoles.Select(role => new Claim(JwtCustomClaimNames.Rol, role)));
 
-			var combinedClaims = new Claim[roleClaims.Length + claims.Length];
-
-			// add first array
-			Array.Copy(claims, combinedClaims, claims.Length);
-
-			// add roleClaims array
-			Array.Copy(roleClaims, 0, combinedClaims, claims.Length, roleClaims.Length);
-
-			// Get Scopes
-			string scopes = "";
-			bool scopesExist = false;
 			if (userRoleClaims.Count > 0)
 			{
-				scopes = string.Join(" ", userRoleClaims);
-				scopesExist = true;
+				var scopes = string.Join(" ", userRoleClaims);
+				claims.Add(new Claim(JwtCustomClaimNames.Scope, scopes));
 			}
 
-
-			// Create the JWT security token and encode it.
 			var jwt = new JwtSecurityToken(
-							_jwtOptions.Issuer,
-							_jwtOptions.Audience,
-							//  Add role scopes if they exist
-							scopesExist ? combinedClaims.Append(new Claim(Constants.Strings.JwtClaimIdentifiers.Scope, scopes)) : combinedClaims,
-							_jwtOptions.NotBefore,
-							_jwtOptions.Expiration,
-							_jwtOptions.SigningCredentials);
+				_jwtOptions.Issuer,
+				_jwtOptions.Audience,
+				claims,
+				_jwtOptions.NotBefore,
+				_jwtOptions.Expiration,
+				_jwtOptions.SigningCredentials
+				);
 
-			return (_jwtTokenHandler.WriteToken(jwt), (int)_jwtOptions.ValidFor.TotalSeconds);
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var tokenString = tokenHandler.WriteToken(jwt);
 
+			return (tokenString, (int)_jwtOptions.ValidFor.TotalSeconds);
 		}
 
 		public Claim GenerateScopeClaim(string resource, string action)
 		{
-			var scope = $"{resource}{scopeClaimSeparator}{action}".ToLower();
-			return new Claim(Constants.Strings.JwtClaimIdentifiers.Scope, scope);
+			var scope = $"{resource}{_scopeClaimSeparator}{action}".ToLower();
+			return new Claim(JwtCustomClaimNames.Scope, scope);
 		}
 
 		public (string, string) DecodeScopeClaim(Claim scopeClaim)
 		{
-			var scopeValues = scopeClaim.Value.Split(scopeClaimSeparator);
+			var scopeValues = scopeClaim.Value.Split(_scopeClaimSeparator);
 
 			return (scopeValues[0], scopeValues[1]);
 		}
 
-		private static ClaimsIdentity GenerateClaimsIdentity(string id, string userName)
-		{
-			return new ClaimsIdentity(
-				new GenericIdentity(userName, "Token"),
-				new[] { new Claim(Constants.Strings.JwtClaimIdentifiers.Id, id), }
-				);
-		}
-
 		private static void ThrowIfInvalidOptions(JwtIssuerOptions options)
 		{
-			//check if null
-			if (options == null) throw new ArgumentNullException(nameof(options));
+			ArgumentNullException.ThrowIfNull(options, nameof(options));
 
-			//check if valid for is set
-			if (options.ValidFor <= TimeSpan.Zero)
-			{
-				throw new ArgumentException("Must be a non-zero TimeSpan.", nameof(JwtIssuerOptions.ValidFor));
-			}
+			ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(options.ValidFor, TimeSpan.Zero, nameof(options.ValidFor));
 
-			//check if Signing Credentials is set
-			if (options.SigningCredentials == null)
-			{
-				throw new ArgumentNullException(nameof(JwtIssuerOptions.SigningCredentials));
-			}
-			//check if Jti Generator i set
-			if (options.JtiGenerator == null)
-			{
-				throw new ArgumentNullException(nameof(JwtIssuerOptions.JtiGenerator));
-			}
+			ArgumentNullException.ThrowIfNull(options.SigningCredentials, nameof(options.SigningCredentials));
 
+			ArgumentNullException.ThrowIfNull(options.JtiGenerator, nameof(options.JtiGenerator));
 		}
 	}
 }
