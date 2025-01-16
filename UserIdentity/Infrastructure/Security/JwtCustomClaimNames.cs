91585a88b5ddf940b72f87f1330e80c772714c84
samuelemwangi <mwangi.s.irungu@gmail.com>
2025-01-17 00:10:22 +0300

    refine JwtFactory class & update copyright

---
+++ UserIdentity/Infrastructure/Security/JwtCustomClaimNames.cs
@@ -0,0 +1,9 @@
+﻿namespace UserIdentity.Infrastructure.Security
+{
+	public struct JwtCustomClaimNames
+	{
+		public const string Rol = "roles";
+		public const string Id = "id";
+		public const string Scope = "scopes";
+	}
+}
