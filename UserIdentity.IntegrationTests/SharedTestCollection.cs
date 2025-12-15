001fc7579c21061317f3b780564f1736cd3d841f
samuelemwangi <mwangi.s.irungu@gmail.com>
2025-12-15 15:54:53 +0300

    [dev] refine integration tests

---
+++ UserIdentity.IntegrationTests/SharedTestCollection.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+
+namespace UserIdentity.IntegrationTests;
+
+[CollectionDefinition("Integration Tests")]
+public class SharedTestCollection : ICollectionFixture<TestingWebAppFactory>
+{
+    // This class has no code, and is never created. Its purpose is simply
+    // to be the place to apply [CollectionDefinition] and all the
+    // ICollectionFixture<> interfaces.
+}
--- UserIdentity.IntegrationTests/TestUtils/ApiRoleSettings.cs
+++ UserIdentity.IntegrationTests/TestUtils/ApiRoleSettings.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Linq;
 
+using PolyzenKit.Common.Utilities;
+
 namespace UserIdentity.IntegrationTests.TestUtils;
 
-internal class ApiRoleSettings
+internal static class ApiRoleSettings
 {
-	public static string ServiceName = "useridentity";
-	public static string RolePrefix = $"{ServiceName}:";
+    public static string ServiceName = "useridentity";
+    public static string RolePrefix = $"{ServiceName}{ZenConstants.SERVICE_ROLE_SEPARATOR}";
 
-	public static string RoleId = Guid.NewGuid().ToString();
-	public static string RoleNameBase = "default";
-	public static string RoleName => $"{RolePrefix}{RoleNameBase}";
+    public static string RoleId = Guid.NewGuid().ToString();
+    public static string RoleNameBase = "user";
+    public static string RoleName => $"{RolePrefix}{RoleNameBase}";
 
-	public static string DefaultRoleBase = "default";
-	public static string DefaultRole => $"{RolePrefix}{DefaultRoleBase}";
-
-	public static string AdminRolesBase = "administrator,super-administrator";
-	public static string AdminRoles => string.Join(',', AdminRolesBase.Split(',').Select(r => $"{RolePrefix}{r.Trim()}"));
+    public static string AdminRolesBase = "admin";
+    public static string AdminRoles => string.Join(',', AdminRolesBase.Split(',').Select(r => $"{RolePrefix}{r.Trim()}"));
 }
