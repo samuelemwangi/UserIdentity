1cc85ba1b7eae02903079e9b14ff99019a2015d1
samuelemwangi <mwangi.s.irungu@gmail.com>
2026-01-18 08:21:47 +0300

    add eddsa helper

---
+++ UserIdentity.IntegrationTests/Certs/EdDsaCertsHelper.cs
@@ -0,0 +1,35 @@
+﻿using PolyzenKit.Common.Utilities;
+
+using UserIdentity.IntegrationTests.TestUtils;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UserIdentity.IntegrationTests.Certs;
+
+public class EdDsaCertsHelper(
+   ITestOutputHelper testOutputHelper
+  )
+{
+  private readonly ITestOutputHelper _testOutputHelper = testOutputHelper;
+
+  [Fact]
+  public void Generating_Certs_Pair_Generates_And_Prints_As_Expected()
+  {
+    var privateKeyPassPhrase = StringUtil.GenerateRandomString(32, true, true);
+
+    var keyPair = EdDsaHelper.GenerateEd25519KeyPair();
+
+    var privateKeyPem = EdDsaHelper.ConvertToPem(keyPair.Private, privateKeyPassPhrase);
+    var publicKeyPem = EdDsaHelper.ConvertToPem(keyPair.Public);
+
+    _testOutputHelper.WriteLine("Private Key Pem");
+    _testOutputHelper.WriteLine(privateKeyPem);
+
+    _testOutputHelper.WriteLine("Private Key Pem Passphrase");
+    _testOutputHelper.WriteLine(privateKeyPassPhrase);
+
+    _testOutputHelper.WriteLine("Private Key Pem");
+    _testOutputHelper.WriteLine(publicKeyPem);
+  }
+}
